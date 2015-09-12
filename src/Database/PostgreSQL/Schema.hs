f0e6ad924c54657baca0b4f5f2fe9febb1c6c7f8
Mark Fine <mark.fine@gmail.com>
2015-09-12 15:37:47 -0700

    incorporate recur.

--- src/Database/PostgreSQL/Schema.hs
+++ src/Database/PostgreSQL/Schema.hs
@@ -23,6 +23,11 @@ import Formatting         ( (%), sformat, stext )
 import Shelly
 
 
+-- types
+
+type Migration = (FilePath, FilePath)
+
+
 -- psql
 
 psqlCommand :: Text -> Text -> Sh Text
@@ -77,37 +82,50 @@ checkSchema schema url = do
   r <- psqlCommand (countSchema schema) url
   return $ strip r == "0"
 
-filterMigrations :: [FilePath] -> Text -> Text -> Text -> Sh [FilePath]
+filterMigrations :: [Migration] -> Text -> Text -> Text -> Sh [Migration]
 filterMigrations migrations table schema url = do
-  r <- psqlCommand (selectMigrations migrations table schema) url
-  return $ migrations \\ map fromText (lines r)
-
-
--- migrations
+  r <- psqlCommand (selectMigrations (map snd migrations) table schema) url
+  return $ removes ((==) . snd) migrations (map fromText (lines r)) where
+    removes p = foldr remove where
+      remove x = foldr f [] where
+        f a b = if p a x then b else a : b
 
 ls_f :: FilePath -> Sh [FilePath]
 ls_f dir = do
   items <- ls dir
   filterM test_f items
 
-findMigrations :: FilePath -> Sh [FilePath]
-findMigrations dir = do
+lsMigrations :: FilePath -> Sh [Migration]
+lsMigrations dir = do
   migrations <- ls_f dir
-  forM migrations $ relativeTo dir
+  migrations' <- forM migrations $ relativeTo dir
+  return $ sortBy (comparing snd) $ zip (repeat dir) migrations'
 
-migrate :: [FilePath] -> Text -> Text -> Text -> Sh ()
+findMigrations :: FilePath -> Sh [Migration]
+findMigrations dir = do
+  dirs <- findWhen test_d dir
+  migrations <- forM (dir : dirs) lsMigrations
+  return $ sortBy (comparing snd) $ concat migrations
+
+searchMigrations :: Bool -> FilePath -> Sh [Migration]
+searchMigrations recur =
+  if recur then findMigrations else lsMigrations
+
+migrate :: [Migration] -> Text -> Text -> Text -> Sh ()
 migrate migrations table schema url =
-  withTmpDir $ \dir ->
-    forM_ migrations $ \migration -> do
+  forM_ migrations $ uncurry $ \dir migration ->
+    chdir dir $ do
       echo $ out migration
       contents <- readfile migration
-      appendfile (dir </> migration) "\\set ON_ERROR_STOP true\n\n"
-      appendfile (dir </> migration) contents
-      appendfile (dir </> migration) $ insertMigration migration table schema
-      psqlFile (dir </> migration) url where
-        out migration =
-          sformat ( "M " % stext % " -> " % stext )
-            (toTextIgnore migration) table
+      withTmpDir $ \dir' ->
+        chdir dir' $ do
+          appendfile migration "\\set ON_ERROR_STOP true\n\n"
+          appendfile migration contents
+          appendfile migration $ insertMigration migration table schema
+          psqlFile migration url where
+            out migration =
+              sformat ( "M " % stext % " -> " % stext )
+                (toTextIgnore migration) table
 
 
 -- API
@@ -115,13 +133,13 @@ migrate migrations table schema url =
 -- | Add a DDL migration file to a migrations directory. Fails if
 -- migration file or migrations directory do not exist.
 add :: FilePath -> FilePath -> FilePath -> Sh ()
-add migration file dir = do
-  echo out
-  mv file (dir </> migration) where
-    out =
-      sformat ( "A " % stext % " -> " % stext )
-        (toTextIgnore file)
-        (toTextIgnore (dir </> migration))
+add migration file dir =
+  chdir dir $ do
+    echo out
+    mv file migration where
+      out =
+        sformat ( "A " % stext % " -> " % stext )
+          (toTextIgnore file) (toTextIgnore migration)
 
 -- | Apply bootstrap migrations to a database. Checks if a database
 -- has been previously bootstrapped, and applies all bootstrap
@@ -130,24 +148,23 @@ add migration file dir = do
 -- their application.
 bootstrap :: FilePath -> Text -> Text -> Text -> Sh ()
 bootstrap dir table schema url = do
-  migrations <- findMigrations dir
-  chdir dir $ do
-    check <- checkSchema schema url
-    when check $ do
-      echo "Bootstrapping..."
-      migrate migrations table schema url
-    migrations' <- filterMigrations migrations table schema url
-    unless (null migrations') $ do
-      echo "Bootstrap migrating..."
-      migrate migrations' table schema url
+  migrations <- lsMigrations dir
+  check <- checkSchema schema url
+  when check $ do
+    echo "Bootstrapping..."
+    migrate migrations table schema url
+  migrations' <- filterMigrations migrations table schema url
+  unless (null migrations') $ do
+    echo "Bootstrap migrating..."
+    migrate migrations' table schema url
+
 
 -- | Apply migrations to a database. Applies all migrations that have
 -- not been applied yet and records their application.
 converge :: Bool -> FilePath -> Text -> Text -> Text -> Sh ()
 converge recur dir table schema url = do
-  migrations <- findMigrations dir
-  chdir dir $ do
-    migrations' <- filterMigrations migrations table schema url
-    unless (null migrations') $ do
-      echo "Migrating..."
-      migrate migrations' table schema url
+  migrations <- searchMigrations recur dir
+  migrations' <- filterMigrations migrations table schema url
+  unless (null migrations') $ do
+    echo "Migrating..."
+    migrate migrations' table schema url
